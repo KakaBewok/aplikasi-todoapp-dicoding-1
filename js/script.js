21dbefe12310a084b2f84c0e37a3968745b6c55d
KkBwk123 <rizalnov667@gmail.com>
2022-01-25 13:59:47 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Fungsi untuk Content Loaded ketika submit diklik
+document.addEventListener("DOMContentLoaded", function () {
+  const submitForm = document.getElementById("form");
+  submitForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    addTodo();
+  });
+});
