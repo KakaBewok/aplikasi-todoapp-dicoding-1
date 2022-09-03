39b9bbb16fcf8e904e3987b48bce34d72f32f4a9
KkBwk <rizalnov667@gmail.com>
2022-09-03 11:48:45 +0700

    first

--- js/DOM.js
+++ js/DOM.js
@@ -1,6 +1,9 @@
+// VARIABEL GLOBAL UNTUK MENYIMPAN ID WADAH YANG BELUM TERISI TODO
 const UNCOMPLETED_LIST_TODO_ID = "todos";
+// VARIABEL GLOBAL UNTUK MENYIMPAN ID WADAH YANG SUDAH TERISI TODO
 const COMPLETED_LIST_TODO_ID = "completed-todos";
 
+// FUNGSI MEMBUAT ELEMEN HTML SEBAGAI WADAH VALUE TO DO
 function makeTodo(data /* string */, timestamp /* string */, isCompleted /* boolean */) {
   const textTitle = document.createElement("h2");
   textTitle.innerText = data;
@@ -17,32 +20,37 @@ function makeTodo(data /* string */, timestamp /* string */, isCompleted /* bool
   container.append(textContainer);
 
   if (isCompleted) {
+    // MENAMPILKAN TOMBOL UNDO DAN HAPUS
     container.append(createUndoButton(), createTrashButton());
   } else {
+    // MENAMPILKAN TOMBOL CEKLIS
     container.append(createCheckButton());
   }
-
+  // KETIKA FUNGSI MAKETODO DIPANGGIL VARIABEL CONTAINER INI YANG AKAN DI RETURN
   return container;
 }
-
+// FUNGSI UNTUK MEMBUAT TOMBOL UNDO
 function createUndoButton() {
   return createButton("undo-button", function (event) {
     undoTaskFromCompleted(event.target.parentElement);
   });
 }
 
+// MEMBUAT TOMBOL HAPUS KE TO DO YANG SUDAH SELESAI
 function createTrashButton() {
   return createButton("trash-button", function (event) {
     removeTaskFromCompleted(event.target.parentElement);
   });
 }
 
+// FUNGSI UNTUK MENANDAI TO DO SEBAGAI SELESAI
 function createCheckButton() {
   return createButton("check-button", function (event) {
     addTaskToCompleted(event.target.parentElement);
   });
 }
 
+// FUNGSI UNTUK MEMBUAT DAN MENAMPILKAN TOMBOL CEKLIS DAN TOMBOL INI MENJADI PATOKAN UNTUK TOMBOL2 YANG LAINNYA
 function createButton(buttonTypeClass /* string */, eventListener /* callback function */) {
   const button = document.createElement("button");
   button.classList.add(buttonTypeClass);
@@ -52,14 +60,18 @@ function createButton(buttonTypeClass /* string */, eventListener /* callback fu
   return button;
 }
 
+// FUNGSI UNTUK MENAMBAHKAN TO DO YANG AKAN DIMASUKAN KE WADAH
 function addTodo() {
   const uncompletedTODOList = document.getElementById(UNCOMPLETED_LIST_TODO_ID);
   const textTodo = document.getElementById("title").value;
   const timestamp = document.getElementById("date").value;
+
   const todo = makeTodo(textTodo, timestamp, false);
 
   uncompletedTODOList.append(todo);
 }
+
+//MENAMBAHKAN TO DO YANG SELESAI
 function addTaskToCompleted(taskElement /* HTMLELement */) {
   const listCompleted = document.getElementById(COMPLETED_LIST_TODO_ID);
   const taskTitle = taskElement.querySelector(".inner > h2").innerText;
@@ -71,10 +83,12 @@ function addTaskToCompleted(taskElement /* HTMLELement */) {
   taskElement.remove();
 }
 
+// MENGHAPUS TO DO YANG SUDAH SELESAI
 function removeTaskFromCompleted(taskElement /* HTMLELement */) {
   taskElement.remove();
 }
 
+// FUNGSI UNTUK MENGEMBALIKAN TO DO SELESAI KE TO DO YANG BELUM SELESAI (TOMBOL UNDO)
 function undoTaskFromCompleted(taskElement /* HTMLELement */) {
   const listUncompleted = document.getElementById(UNCOMPLETED_LIST_TODO_ID);
   const taskTitle = taskElement.querySelector(".inner > h2").innerText;
--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
-// Fungsi untuk Content Loaded ketika submit diklik
 document.addEventListener("DOMContentLoaded", function () {
   const submitForm = document.getElementById("form");
+
   submitForm.addEventListener("submit", function (event) {
     event.preventDefault();
     addTodo();
   });
+});
+
+document.getElementById("title").addEventListener("input", function () {
+  const inputan = document.getElementById("title").value.length;
+  const maks = document.getElementById("title").maxLength;
+  const sesa = maks - inputan;
+  document.getElementById("hitung").innerText = sesa;
+  if (sesa == 0) {
+    document.getElementById("hitung").innerText = "Maximum limit reached!";
+  } else if (sesa <= 5) {
+    document.getElementById("notifKarakter").style.color = "red";
+    document.getElementById("title").style.color = "red";
+  } else {
+    document.getElementById("notifKarakter").style.color = "black";
+    document.getElementById("title").style.color = "black";
+  }
 });
